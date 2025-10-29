ae4c212a7928a18f0e0dc97238d525015177e70e
Ershad Kunnakkadan <ershad92@gmail.com>
2025-10-28 21:43:12 -0400

    Randomize low/mongo queues

--- docker/web/sidekiq_worker.sh
+++ docker/web/sidekiq_worker.sh
@@ -12,8 +12,20 @@ consul_put() {
 
 cd /app
 
-bundle exec sidekiq \
-  -q critical \
-  -q default \
-  -q mongo \
-  -q low
+# Keep critical and default in order, shuffle the rest
+priority_queues=("critical" "default")
+other_queues=("mongo" "low")
+
+# Shuffle only the non-priority queues
+shuffled_other=($(printf "%s\n" "${other_queues[@]}" | shuf))
+
+# Combine priority queues with shuffled queues
+all_queues=("${priority_queues[@]}" "${shuffled_other[@]}")
+
+# Build the sidekiq command
+queue_args=""
+for queue in "${all_queues[@]}"; do
+  queue_args="$queue_args -q $queue"
+done
+
+bundle exec sidekiq $queue_args
